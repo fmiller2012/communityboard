6b53c0baad97ff08579912bf6f5d2d9ef3fe0603
faraji miller <farajimiller@farajis-MacBook-Pro.local>
2025-02-05 12:35:58 -0500

    added images

--- communityboard/src/components/Grid.jsx
+++ communityboard/src/components/Grid.jsx
@@ -9,20 +9,20 @@ const Grid = function() {
   return (
     <div className = "Grid">
       <div className = "container">
-        <ResourceCard card = "Fintech NeoBank" description = "A new way of banking... click to see more" thumbnail = "/neo-bank.jpeg" link = "https://neo-bank.com"/>
-        <ResourceCard card = "Innovation Hub" description = "Tech keys to the future... cick to see more"  thumbnail = "/innovation-hub.jpeg" link = "https://www.thehubmia.com"/>
-        <ResourceCard card = "Business/Cooperative Incubator" description = "Growth and development... click to see more" thumbnail = "/business-incubator.jpeg" link = "https://startup.fiu.edu"/>
-        <ResourceCard card = "Workforce Development" description = "A chicken in every pot; a car in every garage... click to see more" thumbnail = "/workforce-development.jpeg" link = "https://www.miamidade.gov/global/service.page?Mduid_service=ser1556639906576885"/>
+        <ResourceCard card = "Fintech NeoBank" description = "A new way of banking... click to see more" thumbnail = {`${import.meta.env.BASE_URL}neo-bank.jpeg`} link = "https://neo-bank.com"/>
+        <ResourceCard card = "Innovation Hub" description = "Tech keys to the future... cick to see more"  thumbnail = {`${import.meta.env.BASE_URL}innovation-hub.jpeg`} link = "https://www.thehubmia.com"/>
+        <ResourceCard card = "Business/Cooperative Incubator" description = "Growth and development... click to see more" thumbnail = {`${import.meta.env.BASE_URL}business-incubator.jpeg`} link = "https://startup.fiu.edu"/>
+        <ResourceCard card = "Workforce Development" description = "A chicken in every pot; a car in every garage... click to see more" thumbnail = {`${import.meta.env.BASE_URL}workforce-development.jpeg`} link = "https://www.miamidade.gov/global/service.page?Mduid_service=ser1556639906576885"/>
       </div>
       <div className = "container">
-        <ResourceCard card = "K-12 Education" description = "The future belongs to those that prepare today... click to see more" thumbnail = "/k-12.jpeg" link = "https://nmshpioneers.org"/>
-        <ResourceCard card = "Infrastructure" description = "Where the rubber meets the road... click to see more" thumbnail = "/infrastructure.jpeg" link = "https://www.nadaaa.com/portfolio/miami-public-infrastructure/"/>
-        <ResourceCard card = "Housing and Community Development" description = "Home is where the heart is... click to see more" thumbnail = "/community-development.jpeg" link = "https://www.p-scdg.org"/>
-        <ResourceCard card = "Economic Development Headquarters" description = "The heart of it all... click to see more" thumbnail = "/economic-development.png" link = "https://www.eda.gov"/>
+        <ResourceCard card = "K-12 Education" description = "The future belongs to those that prepare today... click to see more" thumbnail = {`${import.meta.env.BASE_URL}k-12.jpeg`} link = "https://nmshpioneers.org"/>
+        <ResourceCard card = "Infrastructure" description = "Where the rubber meets the road... click to see more" thumbnail = {`${import.meta.env.BASE_URL}infrastructure.jpeg`} link = "https://www.nadaaa.com/portfolio/miami-public-infrastructure/"/>
+        <ResourceCard card = "Housing and Community Development" description = "Home is where the heart is... click to see more" thumbnail = {`${import.meta.env.BASE_URL}community-development.jpeg`} link = "https://www.p-scdg.org"/>
+        <ResourceCard card = "Economic Development Headquarters" description = "The heart of it all... click to see more" thumbnail = {`${import.meta.env.BASE_URL}economic-development.png`} link = "https://www.eda.gov"/>
       </div>
       <div className = "container">
-        <ResourceCard card = "Safety and Community Policing" description = "A community is only as strong as it's weakest links... click to see more" thumbnail = "/community-policing.jpeg" link = "https://www.northmiamifl.gov/453/Community-Policing-Section-COPS#:~:text=The%20Community%20Policing%20Section%20(COPS,building%20partnerships%20and%20solving%20problems."/>
-        <ResourceCard card = "Tourism/Hospitality" description = "If you want to get somewhere faster go it alone, if you want to go far go together... click to see more" thumbnail = "/cultural-powerhouse.jpeg" link = "https://www.royalcaribbean.com/?ecid=ps_mdt_lfbrnd_goo_12397&gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADhYZLQ1DJpaIdxkq1IBfS2ni-Zs8&gclid=Cj0KCQiAy8K8BhCZARIsAKJ8sfROb3L0VU8R4xXyCSxPQhO0mn8xprkib4BDOmIj-SX9lPfQUBylGAUaAkZREALw_wcB" />
+        <ResourceCard card = "Safety and Community Policing" description = "A community is only as strong as it's weakest links... click to see more" thumbnail = {`${import.meta.env.BASE_URL}community-policing.jpeg`} link = "https://www.northmiamifl.gov/453/Community-Policing-Section-COPS#:~:text=The%20Community%20Policing%20Section%20(COPS,building%20partnerships%20and%20solving%20problems."/>
+        <ResourceCard card = "Tourism/Hospitality" description = "If you want to get somewhere faster go it alone, if you want to go far go together... click to see more" thumbnail = {`${import.meta.env.BASE_URL}cultural-powerhouse.jpeg`} link = "https://www.royalcaribbean.com/?ecid=ps_mdt_lfbrnd_goo_12397&gclsrc=aw.ds&gad_source=1&gbraid=0AAAAADhYZLQ1DJpaIdxkq1IBfS2ni-Zs8&gclid=Cj0KCQiAy8K8BhCZARIsAKJ8sfROb3L0VU8R4xXyCSxPQhO0mn8xprkib4BDOmIj-SX9lPfQUBylGAUaAkZREALw_wcB" />
       </div>
     </div>
   );
